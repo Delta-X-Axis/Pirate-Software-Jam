89cbe04466081af2557c0ecc038ce781c9019426
T-3N <48658186+T-3N@users.noreply.github.com>
2025-01-19 16:15:45 -0600

    Architecture
    -Added Rudimentary Spell Architecture (Do not ask what this means)

---
+++ Prototypes/Spells/Spell.gd
@@ -0,0 +1,19 @@
+extends Node
+
+@export var cooldown = 0
+@export var duration = 0
+
+var spell_name : String
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func cast():
+	pass
