d0f59841ee845a7ea36e9f54c6a36e353032a82c
T-3N <48658186+T-3N@users.noreply.github.com>
2025-01-31 00:26:47 -0600

    Illsuion treaure cleanup
    Fake treasure now cleans itself up to make the screen less cluttered

--- Spell/Spellbook/Illusory Treasure/IllusoryTreasure.gd
+++ Spell/Spellbook/Illusory Treasure/IllusoryTreasure.gd
@@ -4,11 +4,12 @@ class_name Illusory_Treasure
 @export var ChestScene : PackedScene = preload("res://Spell/Spellbook/Illusory Treasure/Object/Illusion_treasure.tscn")
 var chest
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	duration = 0.05
 	cooldown = 0.5
 	super._ready()
+	
 
 
 func cast():
@@ -17,9 +18,12 @@ func cast():
 	
 	super.cast()
 	
+	
+	
 	chest = ChestScene.instantiate()
 	GameBus.addItem.emit(chest)
 	chest.global_position = target
+	
 	
 	cooldownTimer.start()
 	usable = false
--- Spell/Spellbook/Illusory Treasure/Object/Illusion_Treasure.gd
+++ Spell/Spellbook/Illusory Treasure/Object/Illusion_Treasure.gd
@@ -1,5 +1,14 @@
 extends Node
 
+var exist = timer.new()
+
+func _ready():
+	add_child(exist)
+	exist.wait_time = 1
+	exist.callback.connect(queue_free)
+	exist.start()
 
 func _interact():
 	return 0
+
+
